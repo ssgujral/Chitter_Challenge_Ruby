40200b2d7216e848182769dc4dd109dc0acd6881
samgujral <71089547+samgujral@users.noreply.github.com>
2022-04-09 19:43:59 +0100

    Chitter database data wrapped in program objects

--- lib/cheet.rb
+++ lib/cheet.rb
@@ -1,16 +1,33 @@
 require 'pg'
 
 class Cheet
+
+attr_reader :id, :cheet
+
+def initialize(id:, cheet:)
+  @id = id
+  @cheet = cheet
+end
+
   def self.all
     chitter_database_access_instance = PG.connect(dbname: 'chitter_challenge')
+
     all_cheets = chitter_database_access_instance.exec("SELECT * FROM cheets;")
-    all_cheets.map { |cheet_post| cheet_post['cheet']} 
+    
+    all_cheets.map { |cheet_post| 
+      
+      #cheet_post['cheet']
+      
+      Cheet.new(id: cheet_post['id'], cheet: cheet_post['cheet'])
+    } 
   end
 
-  def self.create(cheet_post:)
+  def self.create(cheet:)
     chitter_database_access = PG.connect(dbname: 'chitter_challenge')
   
-    chitter_database_access.exec("INSERT INTO cheets (cheet) VALUES('#{cheet_post}')")
+    result = chitter_database_access.exec("INSERT INTO cheets (cheet) VALUES('#{cheet}') RETURNING id, cheet")
+
+    Cheet.new(id: result[0]['id'], cheet: result[0]['cheet'])
   end
 
 end
